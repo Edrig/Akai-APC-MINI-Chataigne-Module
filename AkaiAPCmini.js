72a168dfc1ecae5c1f41a70c9c8a4c60d2f35565
Noel Gaetan <gaetan.noel@viacesi.fr>
2022-02-20 21:14:07 +0100

    fix & update
    * move "Pad Color" to values
    * fix trigger midi "Pad Color" value change

--- AkaiAPCmini.js
+++ AkaiAPCmini.js
@@ -2,7 +2,6 @@
 
 function setLed(pad, color)
 {
-	//script.log("SetLed:  "+pad+"/"+colors[0]);
 	local.sendNoteOn(1, pad, colors[0]);
 }
 
@@ -11,63 +10,69 @@ function setLed(pad, color)
 
 function setPadColor(pad, colors)
 {
-	setLed(pad, color[0]);
-
+	//setLed(pad, color[0]);
 	i = parseInt(pad);
-	script.log("a: "+i+"/"+colors[0]);
-	//a = local.parameters.padColors.getChild("pad"+i);
+	//script.log("a: "+i+"/"+colors[0]);
 	x = colors[0];
 	if (x == 0)
 	{
-		local.parameters.padColors.getChild("pad"+i).set("Black");
+		local.values.padColors.getChild("pad"+i).set("Black");
 	}
 	else if (x == 1)
 	{
-		local.parameters.padColors.getChild("pad"+i).set("Green");
+		local.values.padColors.getChild("pad"+i).set("Green");
 	}
 	else if (x == 2)
 	{
-		local.parameters.padColors.getChild("pad"+i).set("Green_Blink");
+		local.values.padColors.getChild("pad"+i).set("Green_Blink");
 	}
 	else if (x == 3)
 	{
-		local.parameters.padColors.getChild("pad"+i).set("Red");	
+		local.values.padColors.getChild("pad"+i).set("Red");	
 	}
 	else if (x == 4)
 	{
-		local.parameters.padColors.getChild("pad"+i).set("Red_Blink");
+		local.values.padColors.getChild("pad"+i).set("Red_Blink");
 	}
 	else if (x == 5)
 	{
-		local.parameters.padColors.getChild("pad"+i).set("Yellow");
+		local.values.padColors.getChild("pad"+i).set("Yellow");
 	}
 	else if (x == 6)
 	{
-		local.parameters.padColors.getChild("pad"+i).set("Yellow_Blink");
+		local.values.padColors.getChild("pad"+i).set("Yellow_Blink");
 	}
-		
 }
+	
+
 
 function resetColors()
 {
-	for(var i=0;i<63;i++) setLed(i, 0);;
+	for(var i=0;i<63;i++) 
+		{
+			setPadColor(i, 0);
+		}
+
 }
 
 //Events
 
 function moduleParameterChanged(param)
 {
-	if(param.getParent().name == "padColors")
-	{
-		var id = parseInt(param.name.substring(3, 5));
-		var val = param.get();
-		script.log("Test: "+param.name.substring(3, 5)+"/"+val[0]+"/"+val[1]);
-		local.sendNoteOn(1, param.name.substring(3, 5), val[1]);
-	}
+  script.log(value.name + " param changed, new value: " + param.get());
 }
 
 function moduleValueChanged(value) {
   script.log(value.name + " value changed, new value: " + value.get());
+  	if(value.getParent().name == "padColors")
+	{
+		var id = parseInt(value.name.substring(3, 5));
+		var val = value.get();
+		script.log("Test: "+value.name.substring(3, 5)+"/"+val[0]+"/"+val[1]);
+		//setLed(value.name.substring(3, 5), val[0]);
+		local.sendNoteOn(1, value.name.substring(3, 5), val[0]);
+
+	}
 }
 
 function noteOnEvent(channel, pitch, velocity)
