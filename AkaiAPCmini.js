a60657a56757f010e080e5bbf31edd549545281e
Noel Gaetan <gaetan.noel@viacesi.fr>
2022-02-20 19:48:18 +0100

    add feature
    + Pad Colors
    + Values Pad

--- AkaiAPCmini.js
+++ AkaiAPCmini.js
@@ -1,9 +1,4 @@
-
-//local.sendNoteOn(1, C0, 2);
-
-    //var awnser = local.values.pads.pad1;
-
- //Functions
+//Functions
 
 function setLed(pad, color)
 {
@@ -18,6 +13,39 @@ function setPadColor(pad, colors)
 {
 	setLed(pad, color[0]);
 
+	i = parseInt(pad);
+	script.log("a: "+i+"/"+colors[0]);
+	//a = local.parameters.padColors.getChild("pad"+i);
+	x = colors[0];
+	if (x == 0)
+	{
+		local.parameters.padColors.getChild("pad"+i).set("Black");
+	}
+	else if (x == 1)
+	{
+		local.parameters.padColors.getChild("pad"+i).set("Green");
+	}
+	else if (x == 2)
+	{
+		local.parameters.padColors.getChild("pad"+i).set("Green_Blink");
+	}
+	else if (x == 3)
+	{
+		local.parameters.padColors.getChild("pad"+i).set("Red");	
+	}
+	else if (x == 4)
+	{
+		local.parameters.padColors.getChild("pad"+i).set("Red_Blink");
+	}
+	else if (x == 5)
+	{
+		local.parameters.padColors.getChild("pad"+i).set("Yellow");
+	}
+	else if (x == 6)
+	{
+		local.parameters.padColors.getChild("pad"+i).set("Yellow_Blink");
+	}
+		
 }
 
 function resetColors()
@@ -25,19 +53,17 @@ function resetColors()
 	for(var i=0;i<63;i++) setLed(i, 0);;
 }
 
-
 //Events
 
 function moduleParameterChanged(param)
 {
 	if(param.getParent().name == "padColors")
 	{
-		var id = parseInt(param.name.substring(3));
+		var id = parseInt(param.name.substring(3, 5));
 		var val = param.get();
-		//script.log("Test: "+val[0]+"/"+val[1]);
-		local.sendNoteOn(1, val[0], val[1]);
+		script.log("Test: "+param.name.substring(3, 5)+"/"+val[0]+"/"+val[1]);
+		local.sendNoteOn(1, param.name.substring(3, 5), val[1]);
 	}
-
 }
 
 function moduleValueChanged(value) {
@@ -48,8 +74,7 @@ function noteOnEvent(channel, pitch, velocity)
 {
 	script.log("Note on received "+channel+", "+pitch+", "+velocity);
 	i = pitch;
-    local.values.pads.getChild("pad"+i).set(1);
-    script.log(i);
+    local.values.pads.getChild("pad" + i).set(1);
 }
 
 
@@ -57,7 +82,7 @@ function noteOffEvent(channel, pitch, velocity)
 {
 	script.log("Note off received "+channel+", "+pitch+", "+velocity);
 	i = pitch;
-    local.values.pads.getChild("pad"+i).set(0);
+    local.values.pads.getChild("pad" + i).set(0);
 }
 
 function ccEvent(channel, number, value)
